b4f0ee13d67ed6238bfc0b42c0692261cf1575ab
Julian Cabrera <julian.cabreras@gmail.com>
2020-01-18 17:13:12 -0600

    Section 7 Reactive programming

---
+++ src/test/java/com/learnreactivespriing/learnreactivespring/fluxandmonoplayground/FluxAndMonoBackPressureTest.java
@@ -0,0 +1,44 @@
+package com.learnreactivespriing.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class FluxAndMonoBackPressureTest {
+
+    @Test
+    public void backPressureTest(){
+        Flux<Integer> finiteFlux =  Flux.range(1,10)
+                .log();
+
+        StepVerifier.create(finiteFlux)
+                .expectSubscription()
+                .thenRequest(1)
+                .expectNext(1)
+                .thenRequest(1)
+                .expectNext(2)
+                .thenCancel()
+                .verify();
+    }
+
+    @Test
+    public void customized_backPressure(){
+        Flux<Integer> finiteFlux = Flux.range(1,10)
+                .log();
+
+        finiteFlux.subscribe(new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnNext(Integer value) {
+                request(1);
+                System.out.println("Value received is : "+value);
+                if(value ==4){
+                    cancel();
+                }
+            }
+        });
+    }
+
+
+
+}
